ba2518afb8ccbb8d1c9281d106c668e48d2a0745
kruthikakolume <114697367+kruthikakolume@users.noreply.github.com>
2024-02-22 20:59:43 -0500

    Revert "Deploy React app to GitHub Pages"

---
+++ src/data/experienceData.js
@@ -0,0 +1,56 @@
+import neulogo from '../assets/png/neulogo.png';
+import see from '../assets/png/see.png';
+import mercedes from '../assets/png/mercedes.jpeg';
+
+
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Northeastern University, Boston, MA',
+        jobtitle: 'Graduate Teaching Assistant',
+        startYear: 'Jan 2024',
+        endYear: 'Present',
+        logo: neulogo,
+        content: 'Guiding and designing quizzes and assignments for 70+ students on UFT Micro Focus, Selenium automation testing, SDLC and QA for the course Software Quality Control & Management.'
+    },
+    {
+        id: 2,
+        company: 'Sealed Air Corporation, Charlotte, NC',
+        jobtitle: 'Software Engineering Intern',
+        startYear: 'May 2023',
+        endYear: 'Aug 2023',
+        jobrole: 'Full Stack Developer',
+        logo: see,
+        content: 'Executed the development of a proof of concept utilizing AEM Headless, integrating a React App with Progressive Web App (PWA) features, resulting in significant enhancements to the company\'s website.'
+    },
+    {
+        id: 3,
+        company: 'Mercedes-Benz R&D India',
+        jobtitle: 'Consultant',
+        startYear: 'Mar 2022',
+        endYear: 'Jul 2022',
+        jobrole: 'UI Developer',
+        logo: mercedes,
+        content: 'Transformed class components to functional components in React and TypeScript to simplify the code structure and boost the user base by 10% with the introduction of a new user design. Skilled in deploying projects using Jenkins for successful launch.'
+    },
+    {
+        id: 4,
+        company: 'Mercedes-Benz R&D India',
+        jobtitle: 'Graduate Engineer Trainee',
+        startYear: 'Aug 2021',
+        endYear: 'Feb 2022',
+        jobrole: 'UI Developer',
+        logo: mercedes,
+        content: 'Performed code modifications to assist with the migration of source data during the shutdown of the source system. Obtained expertise in using React, TypeScript and Angular to create interactive User Interfaces for improved user satisfaction.'
+    },
+    {
+        id: 5,
+        company: 'Mercedes-Benz R&D India',
+        jobtitle: 'Student Intern',
+        startYear: 'Mar 2021',
+        endYear: 'Jul 2021',
+        jobrole: 'UI Developer',
+        logo: mercedes,
+        content: 'Developed a comprehensive information portal, showcasing information on various car components on a single dashboard, to aid the research and development team in designing parts using HTML, CSS, TypeScript, and React.'
+    },
+]
